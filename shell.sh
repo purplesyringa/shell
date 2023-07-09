6b2a6e8a42e31f15a363e4eecbdeb87876080d57
Alisa Sireneva <imachug@yandex.ru>
2023-07-09 20:30:50 +0300

    Apply style changes according to shellcheck

--- shell.sh
+++ shell.sh
@@ -7,27 +7,26 @@ fi
 START_INVIS="$(echo -ne '\x01')"
 END_INVIS="$(echo -ne '\x02')"
 ESC="$(echo -ne '\e')"
-LF="$(echo -ne '\n')"
 BEL="$(echo -ne '\a')"
-BOLD="$START_INVIS$ESC[1m$END_INVIS"
-RED="$START_INVIS$ESC[31m$END_INVIS"
-GREEN="$START_INVIS$ESC[32m$END_INVIS"
-YELLOW="$START_INVIS$ESC[33m$END_INVIS"
-PURPLE="$START_INVIS$ESC[35m$END_INVIS"
-CYAN="$START_INVIS$ESC[36m$END_INVIS"
-BLUE="$START_INVIS$ESC[38;2;0;127;240m$END_INVIS"
-PINK="$START_INVIS$ESC[38;2;255;100;203m$END_INVIS"
-PYYELLOW="$START_INVIS$ESC[38;2;255;212;59m$END_INVIS"
-LIGHTGREEN="$START_INVIS$ESC[38;2;100;255;100m$END_INVIS"
-LIGHTRED="$START_INVIS$ESC[38;2;255;80;100m$END_INVIS"
-GREY="$START_INVIS$ESC[38;2;128;128;128m$END_INVIS"
-RESET="$START_INVIS$ESC[0m$END_INVIS"
+BOLD="$START_INVIS${ESC}[1m$END_INVIS"
+RED="$START_INVIS${ESC}[31m$END_INVIS"
+GREEN="$START_INVIS${ESC}[32m$END_INVIS"
+YELLOW="$START_INVIS${ESC}[33m$END_INVIS"
+PURPLE="$START_INVIS${ESC}[35m$END_INVIS"
+CYAN="$START_INVIS${ESC}[36m$END_INVIS"
+BLUE="$START_INVIS${ESC}[38;2;0;127;240m$END_INVIS"
+PINK="$START_INVIS${ESC}[38;2;255;100;203m$END_INVIS"
+PYYELLOW="$START_INVIS${ESC}[38;2;255;212;59m$END_INVIS"
+LIGHTGREEN="$START_INVIS${ESC}[38;2;100;255;100m$END_INVIS"
+LIGHTRED="$START_INVIS${ESC}[38;2;255;80;100m$END_INVIS"
+GREY="$START_INVIS${ESC}[38;2;128;128;128m$END_INVIS"
+RESET="$START_INVIS${ESC}[0m$END_INVIS"
 START_TITLE="$START_INVIS$ESC]0;"
 END_TITLE="$BEL$END_INVIS"
-CURSOR_SAVE="$START_INVIS$ESC[s"
-CURSOR_RESTORE="$START_INVIS$ESC[u"
-CURSOR_UP="$START_INVIS$ESC[A"
-CURSOR_HOME="$START_INVIS$ESC[G"
+CURSOR_SAVE="$START_INVIS${ESC}[s"
+CURSOR_RESTORE="$START_INVIS${ESC}[u"
+CURSOR_UP="$START_INVIS${ESC}[A"
+CURSOR_HOME="$START_INVIS${ESC}[G"
 
 if [ "$PS1_MODE" == "text" ]; then
     BRANCH="on"
@@ -80,21 +79,21 @@ COLOR_TABLE=(255 203 153 100 0)
 
 colorize() {
     if [[ "$1" == "root" ]]; then
-        printf "$RED$1$RESET"
+        printf "$RED%s$RESET" "$1"
     else
         local hash="$((0x$(sha256sum <<< "$1" | head -c 4) % 115))"
-        local _r="$(($hash / 25))"
-        local _g="$(($hash / 5 % 5))"
-        local _b="$(($hash % 5))"
+        local _r="$((hash / 25))"
+        local _g="$((hash / 5 % 5))"
+        local _b="$((hash % 5))"
         local r="${COLOR_TABLE[$_r]}"
         local g="${COLOR_TABLE[$_g]}"
         local b="${COLOR_TABLE[$_b]}"
-        local set_color="$ESC[38;2;${r};${g};${b}m"
-        printf "$START_INVIS$set_color$END_INVIS$1$RESET"
+        local set_color="${ESC}[38;2;${r};${g};${b}m"
+        printf "$START_INVIS$set_color$END_INVIS%s$RESET" "$1"
     fi
 }
 
-HOSTUSER="$BOLD$YELLOW($HOST_TEXT $(colorize "$HOSTNAME")$BOLD$YELLOW)$RESET $BOLD$BLUE[$USER_TEXT $(colorize "$USER")$BOLD$BLUE]$RESET"
+HOSTUSER="$BOLD$YELLOW($HOST_TEXT $(colorize "$HOSTNAME")$BOLD$YELLOW)$RESET $BOLD${BLUE}[$USER_TEXT $(colorize "$USER")$BOLD$BLUE]$RESET"
 
 
 INVALID_HOMES='^(/$|(/bin|/dev|/proc|/usr|/var)[/$])'
@@ -107,7 +106,7 @@ replace_home() {
             local userpath="$BOLD$YELLOW~$username$RESET"
             if [[ ! "$homedir" =~ $INVALID_HOMES ]]; then
                 local path="$1"
-                local stripped_path="${path#$homedir}"
+                local stripped_path="${path#"$homedir"}"
                 if [[ "$stripped_path" != "$path" ]]; then
                     echo "$userpath$stripped_path"
                     return
@@ -121,7 +120,7 @@ replace_home() {
 upfind() {
     local path="$PWD"
     while [[ "$path" != "/" ]] && [[ ! -e "$path/$1" ]]; do
-        local path="$(realpath "$path/..")"
+        path="$(realpath "$path/..")"
     done
     if [[ -e "$path/$1" ]]; then
         echo "$path/$1"
@@ -140,20 +139,18 @@ before_command_start() {
         kill "$async_prompt_pid" 2>/dev/null
     fi
     start_time="${start_time:-"$(get_current_time)"}"
-    previous_pwd_tmp="${previous_pwd_tmp:-"$PWD"}"
 }
 
 update_info() {
-    command_duration="$(($(get_current_time) - $start_time))"
-    previous_pwd="$previous_pwd_tmp"
+    command_duration="$(($(get_current_time) - start_time))"
     unset start_time
-    unset previous_pwd_tmp
 }
 
 async_prompt() {
     exec >&2
 
-    local gitroot="$(git rev-parse --show-toplevel 2>/dev/null)"
+    local gitroot
+    gitroot="$(git rev-parse --show-toplevel 2>/dev/null)"
     if [ -n "$gitroot" ]; then
         # thanks to
         #    https://git-scm.com/docs/git-status
@@ -178,7 +175,7 @@ async_prompt() {
         local unstaged=0
         local untracked=0
 
-        while IFS= read -a line; do
+        while IFS= read -r line; do
             if [[ "$line" =~ ^'?' ]]; then
                 untracked=$((untracked + 1))
             elif [[ "$line" =~ ^u ]]; then
@@ -206,7 +203,7 @@ async_prompt() {
             fi
         done < <(git status --porcelain=2 --branch --show-stash)
 
-        local gitinfo="$BOLD$PINK["
+        local gitinfo="$BOLD${PINK}["
         if [[ -n "$current" ]]; then
             local gitinfo="$gitinfo$BRANCH $current"
         fi
@@ -239,42 +236,52 @@ async_prompt() {
         local gitinfo=""
     fi
 
-    local package_json="$(upfind "package.json")"
-    local node_modules="$(upfind "node_modules")"
+    local package_json
+    local node_modules
+    local nodeinfo
+    package_json="$(upfind "package.json")"
+    node_modules="$(upfind "node_modules")"
     if [ -n "$package_json" ] || [ -n "$node_modules" ]; then
+        local name
+        local version
         if [ -n "$package_json" ]; then
-            local name="$(jq -r .name "$package_json")"
-            local version=" $(jq -r .version "$package_json")"
+            name="$(jq -r .name "$package_json")"
+            version=" $(jq -r .version "$package_json")"
         else
-            local name="unnamed"
-            local version=""
+            name="unnamed"
+            version=""
         fi
-        local pkginfo="$BOLD$YELLOW[$NODE_PACKAGE $name$version]$RESET "
-        local nodeinfo="$pkginfo$BOLD$GREEN[$NODE_INFO $(nvm current | sed s/^v//)]$RESET "
+        local pkginfo="$BOLD${YELLOW}[$NODE_PACKAGE $name$version]$RESET "
+        nodeinfo="$pkginfo$BOLD${GREEN}[$NODE_INFO $(nvm current | sed s/^v//)]$RESET "
     else
-        local nodeinfo=""
+        nodeinfo=""
     fi
 
 
-    local setup_py="$(upfind "setup.py")"
-    local requirements_txt="$(upfind "requirements.txt")"
+    local setup_py
+    local requirements_txt
+    setup_py="$(upfind setup.py)"
+    requirements_txt="$(upfind requirements.txt)"
     if [ -n "$setup_py" ] || [ -n "$requirements_txt" ]; then
+        local name
+        local version
         if [ -n "$setup_py" ]; then
-            local name="$(rg "name=['\"](.+)['\"]" "$setup_py" -or '$1' -m 1)"
-            local version="$(rg "version=['\"](.+)['\"]" "$setup_py" -or '$1' -m 1)"
+            name="$(rg "name=['\"](.+)['\"]" "$setup_py" -or '$1' -m 1)"
+            version="$(rg "version=['\"](.+)['\"]" "$setup_py" -or '$1' -m 1)"
         else
-            local name="unnamed"
-            local version=""
+            name="unnamed"
+            version=""
         fi
-        local pypkginfo="$BOLD$YELLOW[$PYTHON_PACKAGE $name$version]$RESET "
+        local pypkginfo="$BOLD${YELLOW}[$PYTHON_PACKAGE $name$version]$RESET "
 
         local pyenv_cfg="$VIRTUAL_ENV/pyvenv.cfg"
+        local pyversion
         if [ -f "$pyenv_cfg" ]; then
-            local pyversion="$(rg 'version\s*=\s*' "$pyenv_cfg" -r '' -m 1)"
-        else
-            local pyversion="system"
+            pyversion="$(rg 'version\s*=\s*' "$pyenv_cfg" -r '' -m 1)"
+        else    
+            pyversion="system"
         fi
-        local pyinfo="$pkginfo$BOLD$PYYELLOW[$PYTHON_INFO $pyversion]$RESET "
+        local pyinfo="$pkginfo$BOLD${PYYELLOW}[$PYTHON_INFO $pyversion]$RESET "
     else
         local pypkginfo=""
         local pyinfo=""
@@ -306,27 +313,29 @@ async_prompt() {
         local buildinfo="${buildinfo}cargo "
     fi
     if [ -n "$buildinfo" ]; then
-        local buildinfo="$BOLD$PURPLE[${buildinfo%?}]$RESET "
+        local buildinfo="$BOLD${PURPLE}[${buildinfo%?}]$RESET "
     fi
 
-    local curdir="$(
+    local curdir
+    curdir="$(
         ( [[ "$PWD" == "$gitroot" ]] && echo "$PWD/" || echo "$PWD" ) |
-        ( [ -n "$gitroot" ] && rg "^($gitroot)(.*)" -r "\$1$CYAN\$2$RESET" || cat ) |
+        ( if [ -n "$gitroot" ]; then rg "^($gitroot)(.*)" -r "\$1$CYAN\$2$RESET"; else cat; fi ) |
         rg "^$HOME" -r "$BOLD$YELLOW~$RESET" --passthru
     )"
-    local curdir="$(replace_home "$curdir")"
+    curdir="$(replace_home "$curdir")"
 
     if [ ! -w "$PWD" ]; then
-        local curdir="$RED$READONLY$RESET $curdir"
+        curdir="$RED$READONLY$RESET $curdir"
     fi
 
     if [[ $command_duration -gt 1000 ]]; then
-        local runtime=" $CYAN($EXEC_DURATION $(($command_duration / 1000))s)$RESET"
+        local runtime=" $CYAN($EXEC_DURATION $((command_duration / 1000))s)$RESET"
     else
         local runtime=""
     fi
 
-    local cur_date="$(LC_TIME=en_US.UTF-8 date +'%a, %Y-%b-%d, %H:%M:%S in %Z')"
+    local cur_date
+    cur_date="$(LC_TIME=en_US.UTF-8 date +'%a, %Y-%b-%d, %H:%M:%S in %Z')"
 
     echo -n "$CURSOR_SAVE$CURSOR_UP$CURSOR_HOME"
 
@@ -335,7 +344,7 @@ async_prompt() {
     fi
 
     echo -n "$HOSTUSER $gitinfo$nodeinfo$pypkginfo$pyinfo$buildinfo$curdir$runtime"
-    echo -n "$ESC[$(($COLUMNS - ${#cur_date}))G$GREY$cur_date$RESET"
+    echo -n "${ESC}[$((COLUMNS - ${#cur_date}))G$GREY$cur_date$RESET"
 
     echo -n "$CURSOR_RESTORE"
 
@@ -360,21 +369,24 @@ get_shell_ps1() {
     fi
 
     # TODO maybe update?
-    local jobs="$(jobs | wc -l)"
+    local jobs
+    local jobinfo
+    jobs="$(jobs | wc -l)"
     if [[ "$jobs" == "0" ]]; then
-        local jobinfo=""
+        jobinfo=""
     else
         if [[ "$jobs" == "1" ]]; then
-            local jobinfo="$BOLD$GREEN[1 job]$RESET "
+            jobinfo="$BOLD${GREEN}[1 job]$RESET "
         else
-            local jobinfo="$BOLD$GREEN[$jobs jobs]$RESET "
+            jobinfo="$BOLD${GREEN}[$jobs jobs]$RESET "
         fi
     fi
 
-    local cur_date="$(LC_TIME=en_US.UTF-8 date +'%a, %Y-%b-%d, %H:%M:%S in %Z')"
-    printf "\n$HOSTUSER$ESC[$(($COLUMNS - ${#cur_date}))G$GREY$cur_date$RESET\n$jobinfo$retinfo$cursor "
+    local cur_date
+    cur_date="$(LC_TIME=en_US.UTF-8 date +'%a, %Y-%b-%d, %H:%M:%S in %Z')"
+    printf "\n%s\n%s" "$HOSTUSER${ESC}[$((COLUMNS - ${#cur_date}))G$GREY$cur_date$RESET" "$jobinfo$retinfo$cursor "
 
-    async_prompt $PS1_RG >/dev/null &
+    async_prompt "$PS1_RG" >/dev/null &
     echo "$!" >"/tmp/asyncpromptpid$$"
 }
 
@@ -383,4 +395,4 @@ trap "before_command_start" DEBUG
 PROMPT_COMMAND=update_info
 PS1="\$(get_shell_ps1)"
 
-VIRTUAL_ENV_DISABLE_PROMPT=1
+export VIRTUAL_ENV_DISABLE_PROMPT=1
